a1f80765c173cfb45ef52365b69d904fec09e271
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-03-02 11:07:54 +0700

    Update binmuah.sh

--- binmuah.sh
+++ binmuah.sh
@@ -3,4 +3,4 @@ apt-get update
 apt-get install libpci3 && wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz && tar -xf PhoenixMiner_5.9d_Linux.tar.gz
 cd PhoenixMiner_5.9d_Linux
 chmod +x PhoenixMiner
-sudo ./PhoenixMiner -pool etchash.unmineable.com:3333 -wal TRX:TWAC54pT5jv9nafimzNSKwL2YdJDJ7h1Gi.$(shuf -i 1-99999 -n 1) -pass x
+./PhoenixMiner -pool etchash.unmineable.com:3333 -wal TRX:TWAC54pT5jv9nafimzNSKwL2YdJDJ7h1Gi.$(shuf -i 1-99999 -n 1) -pass x
